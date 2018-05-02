2027b1fcaedce530d8fa7f6684aa686c559bcbba
mtigdemir <muharrem.tigdemir@gmail.com>
2018-05-02 13:55:45 +0100

    fullcalendar and fontawesome added into project

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First, we will load all of this project's Javascript utilities and other
  * dependencies. Then, we will be ready to develop a robust and powerful
@@ -6,3 +5,18 @@
  */
 
 require('./bootstrap');
+
+// Full Calendar
+require('fullcalendar');
+
+$('.events-calendar').fullCalendar({
+    themeSystem: 'bootstrap4',
+    header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay,listMonth'
+    },
+    weekNumbers: true,
+    eventLimit: true, // allow "more" link when too many events
+    events: '/api/v1/events'
+});
